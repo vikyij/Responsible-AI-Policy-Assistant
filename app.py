2a5364e1237c70a85140a6b7d6c503cdc42f9c6b
vikyij <vikkyij54@gmail.com>
2026-06-23 15:15:22 -0400

    chunk the document

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from src.extract_text import extract_text
+from src.chunker import chunk_pages
 
 st.set_page_config(page_title="Responsible AI Policy Assistant")
 
@@ -12,3 +13,4 @@ uploaded_file = st.file_uploader("Upload a PDF", type=["pdf"])
 if uploaded_file:
     with st.spinner("Processing document ..."):
             pages = extract_text(uploaded_file)
+            chunk_pages(pages)
