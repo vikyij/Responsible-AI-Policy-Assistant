76749d892cb8a453d11ea046114a62c0a86815e5
vikyij <vikkyij54@gmail.com>
2026-06-26 11:42:03 -0400

    implement question answering; embed question and retreive similar docs,use qwen chat model, supply user question and context to model, display answer

---
+++ src/chat_model.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from huggingface_hub import InferenceClient
+
+load_dotenv()
+
+client = InferenceClient(
+    api_key=os.getenv("HF_TOKEN")
+)
+
+
+def generate_answer(question, context):
+    system_prompt = """
+        You are a Responsible AI Policy Assistant.
+
+        Answer the user's question using only the context provided.
+        If the answer is not in the context, say: "I could not find enough information in the document."
+        """
+    
+    user_prompt = f"""
+        Context: {context}
+        Question: {question}
+    """
+
+
+    response = client.chat.completions.create(
+        model="Qwen/Qwen2.5-7B-Instruct",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ],
+        max_tokens=500
+    )
+
+    return response.choices[0].message.content
